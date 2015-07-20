2d525d9974bacf75b58d20da2cec59e63defe820
Andrew Heebum Kwak <kh-1143@hanmail.net>
2015-07-20 10:08:16 +0900

    init (delete dir)

---
+++ clean.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+rm -rf aclocal.m4
+rm -rf autom4te.cache
+rm -rf compile
+rm -rf config.guess
+rm -rf config.log
+rm -rf config.status
+rm -rf config.sub
+rm -rf configure
+rm -rf depcomp
+rm -rf install-sh
+rm -rf libtool
+rm -rf m4
+rm -rf Makefile
+rm -rf Makefile.in
+rm -rf missing
+rm -rf test/Makefile
+rm -rf test/Makefile.in
+rm -rf test/.deps
+rm -rf test/.libs
+rm -rf commonlib/Makefile
+rm -rf commonlib/Makefile.in
+rm -rf commonlib/.deps
+rm -rf commonlib/.libs
